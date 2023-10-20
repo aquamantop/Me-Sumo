b12a68b0c6afcb8f04b3beb8bc5ed592ff7de13a
juan351 <montiverolp@yahoo.com>
2023-10-19 21:54:59 -0300

    Entities Club Amenity Neighborhood Activity

---
+++ backend/ms-clubs/src/main/java/com/mesumo/msclubs/models/Activity.java
@@ -0,0 +1,18 @@
+package com.mesumo.msclubs.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Activity {
+    @Id
+    private Long id;
+}
--- backend/ms-clubs/src/main/java/com/mesumo/msclubs/models/Club.java
+++ backend/ms-clubs/src/main/java/com/mesumo/msclubs/models/Club.java
@@ -1,4 +1,52 @@
 package com.mesumo.msclubs.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Club {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "neighborhoodId")
+    private Neighborhood neighborhood;
+
+    private String address;
+
+    @ManyToMany(
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            },
+            fetch = FetchType.EAGER
+    )
+    @JoinTable(name = "club_activity",
+            joinColumns = @JoinColumn(name = "clubId"),
+            inverseJoinColumns = @JoinColumn(name = "activityId"))
+    private Set<Activity> activities;
+
+    @ManyToMany(
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            },
+            fetch = FetchType.EAGER
+    )
+    @JoinTable(name = "club_amenity",
+            joinColumns = @JoinColumn(name = "clubId"),
+            inverseJoinColumns = @JoinColumn(name = "amenityId"))
+    private Set<Amenity> amenities;
 }
