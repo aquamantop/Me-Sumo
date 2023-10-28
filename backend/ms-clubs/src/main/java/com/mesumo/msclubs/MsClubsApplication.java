4f1b3a52434fc5b0dd61aab6b87a27cfd88a596a
Franco Rampazzo <franco.rodrigo.rampazzo@gmail.com>
2023-10-28 18:44:40 -0300

    feat: [application.yml] I left the configuration for the local database commented. [MsClubsApplication] I left the runner commented. I added finalName in the pom to configure the name of the .jar that is sent to the EC2.

--- backend/ms-clubs/src/main/java/com/mesumo/msclubs/MsClubsApplication.java
+++ backend/ms-clubs/src/main/java/com/mesumo/msclubs/MsClubsApplication.java
@@ -34,51 +34,51 @@ public class MsClubsApplication {
 		SpringApplication.run(MsClubsApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner loadData() {
-		return args -> {
-			// Crear objetos y guardarlos en la base de datos
-			Amenity amenity1 = new Amenity();
-			amenity1.setName("Piscina");
-			Amenity amenity2 = new Amenity();
-			amenity2.setName("Gimnasio");
-
-			Activity activity1 = new Activity();
-			activity1.setName("Futbol");
-			activity1.setType("5");
-			Activity activity2 = new Activity();
-			activity2.setName("Tenis");
-			activity2.setType("2");
-
-			Neighborhood neighborhood1 = new Neighborhood();
-			neighborhood1.setName("Monserrat");
-			neighborhoodRepository.save(neighborhood1);
-
-			Set<Amenity> amenities = new HashSet<>();
-			amenities.add(amenity1);
-			amenities.add(amenity2);
-			amenityRepository.save(amenity1);
-			amenityRepository.save(amenity2);
-
-			Set<Activity> activities = new HashSet<>();
-			activities.add(activity1);
-			activities.add(activity2);
-			activityRepository.save(activity1);
-			activityRepository.save(activity2);
-
-			Club club1 = new Club();
-			club1.setName("Belgrano");
-			club1.setAddress("Av. Cabildo 1234");
-			club1.setNeighborhood(neighborhood1);
-			club1.setActivities(activities);
-			club1.setAmenities(amenities);
-
-			// Guardar en la base de datos
-			clubRepository.save(club1);
-
-
-		};
-	}
+//	@Bean
+//	public CommandLineRunner loadData() {
+//		return args -> {
+//			// Crear objetos y guardarlos en la base de datos
+//			Amenity amenity1 = new Amenity();
+//			amenity1.setName("Piscina");
+//			Amenity amenity2 = new Amenity();
+//			amenity2.setName("Gimnasio");
+//
+//			Activity activity1 = new Activity();
+//			activity1.setName("Futbol");
+//			activity1.setType("5");
+//			Activity activity2 = new Activity();
+//			activity2.setName("Tenis");
+//			activity2.setType("2");
+//
+//			Neighborhood neighborhood1 = new Neighborhood();
+//			neighborhood1.setName("Monserrat");
+//			neighborhoodRepository.save(neighborhood1);
+//
+//			Set<Amenity> amenities = new HashSet<>();
+//			amenities.add(amenity1);
+//			amenities.add(amenity2);
+//			amenityRepository.save(amenity1);
+//			amenityRepository.save(amenity2);
+//
+//			Set<Activity> activities = new HashSet<>();
+//			activities.add(activity1);
+//			activities.add(activity2);
+//			activityRepository.save(activity1);
+//			activityRepository.save(activity2);
+//
+//			Club club1 = new Club();
+//			club1.setName("Belgrano");
+//			club1.setAddress("Av. Cabildo 1234");
+//			club1.setNeighborhood(neighborhood1);
+//			club1.setActivities(activities);
+//			club1.setAmenities(amenities);
+//
+//			// Guardar en la base de datos
+//			clubRepository.save(club1);
+//
+//
+//		};
+//	}
 
 }
 
