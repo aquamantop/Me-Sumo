a014eea1bc3c998ff48bdd34cdef7c28d333ee7f
mef <ferraro.mariaeugenia@gmail.com>
2023-11-02 00:10:29 -0300

    neighboorhood repository, service and controller started

--- backend/ms-users/src/main/java/com/mesumo/msusers/controllers/NeighborhoodController.java
+++ backend/ms-users/src/main/java/com/mesumo/msusers/controllers/NeighborhoodController.java
@@ -3,7 +3,8 @@ package com.mesumo.msusers.controllers;
 import com.mesumo.msusers.exceptions.ResourceAlreadyExistsException;
 import com.mesumo.msusers.exceptions.ResourceNotFoundException;
 import com.mesumo.msusers.models.entities.Activity;
-import com.mesumo.msusers.models.service.IActivityService;
+import com.mesumo.msusers.models.entities.Neighborhood;
+import com.mesumo.msusers.models.service.INeighborhoodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,19 +13,19 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/activity")
+@RequestMapping("/neighborhood")
 @CrossOrigin
-public class ActivityController {
+public class NeighborhoodController {
 
     @Autowired
-    IActivityService activityService;
+    INeighborhoodService neighborhoodService;
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Activity> getById(@PathVariable Long id) {
-        ResponseEntity<Activity> response = null;
+    public ResponseEntity<Neighborhood> getById(@PathVariable Long id) {
+        ResponseEntity<Neighborhood> response = null;
         try{
-            response = new ResponseEntity<>(activityService.findById(id), HttpStatus.OK);
+            response = new ResponseEntity<>(neighborhoodService.findById(id), HttpStatus.OK);
         } catch (ResourceNotFoundException e){
             e.printStackTrace();
             response = new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
@@ -34,10 +35,10 @@ public class ActivityController {
 
 
     @GetMapping("/search-name")
-    public ResponseEntity<Activity> getByName(@RequestParam("name") String name) {
-        ResponseEntity<Activity> response = null;
+    public ResponseEntity<Neighborhood> getByName(@RequestParam("name") String name) {
+        ResponseEntity<Neighborhood> response = null;
         try{
-            response = new ResponseEntity<>(activityService.findByName(name), HttpStatus.OK);
+            response = new ResponseEntity<>(neighborhoodService.findByName(name), HttpStatus.OK);
         } catch (ResourceNotFoundException e){
             e.printStackTrace();
             response = new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
@@ -47,9 +48,9 @@ public class ActivityController {
 
 
     @GetMapping("/")
-    public ResponseEntity<Set<Activity>> getAll() {
-        ResponseEntity<Set<Activity>> response = null;
-        Set<Activity> set = activityService.findAll();
+    public ResponseEntity<Set<Neighborhood>> getAll() {
+        ResponseEntity<Set<Neighborhood>> response = null;
+        Set<Neighborhood> set = neighborhoodService.findAll();
         if(set != null){
             response = new ResponseEntity<>(set, HttpStatus.OK);
         } else {
@@ -60,16 +61,13 @@ public class ActivityController {
 
 
     @PostMapping("/add")
-    public ResponseEntity add(@RequestBody Activity activity) throws ResourceAlreadyExistsException {
+    public ResponseEntity add(@RequestBody Neighborhood neighborhood) throws ResourceAlreadyExistsException {
         ResponseEntity response = null;
 
-        if(activity != null){
-            response = new ResponseEntity(activityService.create(activity), HttpStatus.CREATED);
+        if(neighborhood != null){
+            response = new ResponseEntity(neighborhoodService.create(neighborhood), HttpStatus.CREATED);
         }
 
         return response;
     }
-
-
-
 }
