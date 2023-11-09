4d68fc57275e8957d9cd139d7d9d981d7d69bbbc
Franco Rampazzo <franco.rodrigo.rampazzo@gmail.com>
2023-11-09 19:19:15 -0300

    feat: Change the name of the activity and neighborhood endpoints for disambiguation with the club endpoints. Modify the YAML and POM files to make them ready for uploading to AWS.

--- backend/ms-users/src/main/java/com/mesumo/msusers/controllers/ActivityController.java
+++ backend/ms-users/src/main/java/com/mesumo/msusers/controllers/ActivityController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/activity")
+@RequestMapping("/user-activity")
 @CrossOrigin
 public class ActivityController {
 
--- backend/ms-users/src/main/java/com/mesumo/msusers/controllers/NeighborhoodController.java
+++ backend/ms-users/src/main/java/com/mesumo/msusers/controllers/NeighborhoodController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/neighborhood")
+@RequestMapping("/user-neighborhood")
 @CrossOrigin
 public class NeighborhoodController {
 
