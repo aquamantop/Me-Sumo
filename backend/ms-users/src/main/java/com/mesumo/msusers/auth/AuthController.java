012a3afc6239409564a1fb3e2598000439b93d38
Franco Rampazzo <franco.rodrigo.rampazzo@gmail.com>
2023-11-10 13:02:55 -0300

    Anotacion @CrossOrigin para auth controller

--- backend/ms-users/src/main/java/com/mesumo/msusers/auth/AuthController.java
+++ backend/ms-users/src/main/java/com/mesumo/msusers/auth/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
