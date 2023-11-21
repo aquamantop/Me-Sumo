5b81b6078f925cf037439995793294891587d797
honeybadger2788 <noe.carosella@gmail.com>
2023-11-21 11:01:03 -0300

    fix auth endpoint at register and login

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -42,7 +42,7 @@ export default function Register() {
     const response = await new Promise((resolve) => {
         axios({
           method: "POST",
-          url: 'http://ec2-3-85-198-231.compute-1.amazonaws.com:8081/auth/register',
+          url: 'http://ec2-50-17-115-59.compute-1.amazonaws.com:8090/auth/register',
           headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
