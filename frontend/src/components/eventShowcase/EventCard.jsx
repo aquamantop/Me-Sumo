78947518bcf65c2da27bd47dd9d91fcaa8084c31
mef <ferraro.mariaeugenia@gmail.com>
2023-11-24 01:09:10 -0300

    eventCard api data complete

--- frontend/src/components/eventShowcase/EventCard.jsx
+++ frontend/src/components/eventShowcase/EventCard.jsx
@@ -6,45 +6,65 @@ import axiosInstance from "../../hooks/api/axiosConfig";
 
 function EventCard({ booking }) {
 
-  const [clubs, setClubs] = useState([]);
-  const [slots, setSlots] = useState([]);
+  //const [clubs, setClubs] = useState([]);
+  //const [slots, setSlots] = useState([]);
+  const [info, setInfo] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   
   useEffect(() => {
-    // axios({
-    //   method: "GET",
-    //   url: "http://ec2-107-21-182-26.compute-1.amazonaws.com:8090/club/",
-    //   headers: {
-    //     Accept: "application/json",
-    //     "Content-Type": "application/json"
-    //   }
+
+    // axiosInstance.get('/club/')
+    // .then((response) => {
+    //   setClubs(response.data)
+    //   setLoading(false)
     // })
+    // .catch((error) => setError(error))
 
-    // const response = axiosInstance.get('/slot/');
-    // const slotIds = response.data.map(slot => slot.id);
-    // console.log(slotIds);
+  //   axiosInstance.get('/slot/')
+  //   .then((response) => {
+  //     const slotIds = response.data.map(item => item.id);
+  //     //setSlots(response.data)
+  //     setLoading(false)
+  //     console.log(slotIds)
+  //     const slotsClub = slotIds.map(async (slotId) => {
+  //         const slotResponse = await axiosInstance.get(`/slot/getWithCourt/${slotId}`);
+  //         const { id, name, url } = slotResponse.data.court.club;
+  //         const availability = slotResponse.data.capacity - booking.participants;
+  //         const cardData = {
+  //           slotId,
+  //           name,
+  //           url,
+  //           capacity}
+  //         console.log(cardData);
+  //         return cardData;
+  //       });
+  //       setSlots(slotsClub)
+  //     //console.log(slotsData);
+  //   })
+  //   .catch((error) => setError(error)) 
 
-    axiosInstance.get('/club/')
+  // }, [])
+
+    console.log(booking.slotId)
+    axiosInstance.get(`/slot/getWithCourt/${booking.slotId}`)
     .then((response) => {
-      setClubs(response.data)
-      setLoading(false)
-    })
-    .catch((error) => setError(error))
-
-    // axios({
-    //   method: "GET",
-    //   url: "http://ec2-107-21-182-26.compute-1.amazonaws.com:8090/slot/",
-    //   headers: {
-    //     Accept: "application/json",
-    //     "Content-Type": "application/json"
-    //   }
-    // })
-    axiosInstance.get('/slot/')
-    .then((response) => {
-      setSlots(response.data)
-      setLoading(false)
-      console.log(response.data)
+      const { name, url } = response.data.court.club;
+      console.log(response.data.capacity)
+      console.log(booking.participants)
+      console.log(response.data.capacity - booking.participants)
+      const availability = response.data.capacity - booking.participants;
+      const cardData = {
+        "clubName": name,
+        "bookingName":booking.name,
+        "bookingDate":booking.date,
+        "bookingstartTime":booking.startTime,
+        "bookingAvailability": availability,
+        "clubUrl":url
+      }
+      setInfo(cardData);
+      console.log(cardData);
+      return cardData;
     })
     .catch((error) => setError(error)) 
 
@@ -64,20 +84,20 @@ function EventCard({ booking }) {
     >
     <CardContent>
       <Typography variant="h6" color="primary.main">
-        Club: Deportivo Test FC
+        Club: {info.clubName}
       </Typography>
-      <Typography variant="h6" color="primary.main">{booking.name}</Typography>
-      <Typography variant="body2" color="secondary.main">Fecha: {booking.date}</Typography>
-      <Typography variant="body2" color="secondary.main">Inicio: {booking.startTime}</Typography>
+      <Typography variant="h6" color="primary.main">{info.bookingName}</Typography>
+      <Typography variant="body2" color="secondary.main">Fecha: {info.bookingDate}</Typography>
+      <Typography variant="body2" color="secondary.main">Inicio: {info.bookingstartTime}</Typography>
       <Typography variant="body2" color="secondary.main">
-        Lugares Disponibles: {booking.participants} 
+        Lugares Disponibles: {info.bookingAvailability} 
       </Typography>
     </CardContent>
     <CardMedia
       component="img"
       alt="Imagen del Evento"
       height="200"
-      image={eventoImagen}
+      image={info.clubUrl}
     />
     <Box
       sx={{
