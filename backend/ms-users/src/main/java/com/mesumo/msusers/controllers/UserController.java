33ee2f53a3c03c58f3994b1a0fe0e759fb37af0b
Franco Rampazzo <franco.rodrigo.rampazzo@gmail.com>
2023-11-27 23:22:44 -0300

    imports no usados

--- backend/ms-users/src/main/java/com/mesumo/msusers/controllers/UserController.java
+++ backend/ms-users/src/main/java/com/mesumo/msusers/controllers/UserController.java
@@ -5,7 +5,6 @@ import com.mesumo.msusers.exceptions.ResourceNotFoundException;
 import com.mesumo.msusers.models.entities.User;
 import com.mesumo.msusers.models.service.IUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
