aaa9379a090b9186793add4f5d58a61116ba672a
mef <ferraro.mariaeugenia@gmail.com>
2023-11-28 20:36:06 -0300

    message box styles

--- frontend/src/components/message.jsx
+++ frontend/src/components/message.jsx
@@ -8,36 +8,7 @@ import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 
 
-// const Alert = React.forwardRef(function Alert(props, ref) {
-//     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-//   });
-
 const NotLoggedInMessage = ({ open, message, onClose }) => {
-
-    // const rectangleStyle = {
-    //     position: 'fixed',
-    //     top: '50%',
-    //     left: '50%',
-    //     transform: 'translate(-50%, -50%)',
-    //     padding: '20px',
-    //     background: 'linear-gradient(#6D006B, #0D2430)',
-    //     color: '#fff',
-    //     borderRadius: '8px',
-    //     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-    //     display: open ? 'block' : 'none',
-    //     zIndex: 9999,
-    // };    
-
-
-//  return 
-//   (
-//     <div style={rectangleStyle}>
-//     <div>{message}</div>
-//     <button onClick={onClose} style={{ marginTop: '10px' }} sx={{ ...ButtonSX }}>
-//     Cerrar
-//     </button>
-//     </div>
-//   );
 
 return (
     <Dialog open={open} onClose={onClose} maxWidth='lg' >
@@ -71,6 +42,5 @@ return (
   );
 };
 
-//};
 
 export default NotLoggedInMessage;
