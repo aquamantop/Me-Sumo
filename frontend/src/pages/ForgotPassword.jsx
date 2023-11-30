a3a63726468692c2e9a3b2b8200da7b55bb85e02
honeybadger2788 <noe.carosella@gmail.com>
2023-11-30 14:04:00 -0300

    create forgotPassword view

---
+++ frontend/src/pages/ForgotPassword.jsx
@@ -0,0 +1,112 @@
+import EmailSharpIcon from "@mui/icons-material/EmailSharp"
+import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+import Stack from "@mui/material/Stack"
+import Typography from "@mui/material/Typography"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import CustomInput from "../components/customInput/CustomInput"
+import { useNavigate } from "react-router-dom"
+import axiosInstance from "../hooks/api/axiosConfig";
+import { ButtonSX } from '../components/customMui/CustomMui'
+
+export default function ForgotPassword() {
+  const navigate = useNavigate();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors }
+  } = useForm({
+    defaultValues: {
+      email: "",
+    }
+  })
+
+  const [error, setError] = useState("")
+
+
+  const onSubmit = handleSubmit(async (userData) => {
+    
+    // TODO - AGREGAR EL ENDPOINT PARA CAMBIAR LA CONTRASEÑA
+    /* const response = await new Promise((resolve) => {
+      axiosInstance.post("/auth/login", userData)
+        .then((response) => resolve(response))
+        .catch((error) => setError(error))
+    }) */
+  })
+
+  return (
+    <>
+      
+      <Box
+        backgroundColor="background.paper"
+        align="center"
+        className="content"
+      >
+        <Typography
+          variant="h4"
+          color="primary.main"
+          sx={{
+            letterSpacing: 13
+          }}
+        >
+          Restablecer Contraseña
+        </Typography>
+        <Typography
+          variant="h6"
+          color="primary.main"
+          sx={{
+            fontWeight: "regular",
+            letterSpacing: " 4.68px",
+            marginBottom: "36px"
+          }}
+        >
+          Ingresa tu correo electrónico y te enviaremos un enlace para restablecer tu contraseña.
+        </Typography>
+        <Stack
+          sx={{ margin: "auto", px: 5 }}
+          component="form"
+          maxWidth={500}
+          spacing={2}
+          m={2}
+          onSubmit={onSubmit}
+        >
+          <CustomInput
+            name="email"
+            control={control}
+            placeholder="Email"
+            error={!!errors.email}
+            helperText={errors?.email?.message}
+            type="email"
+            rules={{
+              required: {
+                value: true,
+                message: "El correo es requerido"
+              },
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                message: "Correo no válido"
+              }
+            }}
+            icon={<EmailSharpIcon />}
+          />
+
+          <Button
+            variant='contained'
+            type='submit'
+            sx={{...ButtonSX}}
+          >
+            Enviar mail
+          </Button>
+
+          {error && (
+            <Typography variant="body2" color="error.main">
+              Algo salió mal. Por favor, vuelva a intentarlo
+            </Typography>
+          )}
+        </Stack>
+      </Box>
+    </>
+  )
+}
