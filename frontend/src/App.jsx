8ef017f411aa5f3efc38e0ce498bca606ed3eb1c
honeybadger2788 <noe.carosella@gmail.com>
2023-12-05 19:32:19 -0300

    fix imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,98 +1,97 @@
-import { ThemeProvider, createTheme } from '@mui/material'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { Route, Routes } from 'react-router'
-import './App.css'
-import Club from './pages/Club'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import LoginSuccess from './pages/LoginSuccess'
-import Register from './pages/Register'
-import EventCreate from './pages/EventCreate'
-import Booking from './pages/Event'
-import { UserProvider } from './hooks/userContext'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
-import { BookingProvider } from './hooks/bookingContext'
-import Profile from './pages/Profile'
-import Slots from './pages/Slots'
-import Bookings from './pages/Bookings'
-import ForgotPassword from './pages/ForgotPassword'
-import ResetPassword from './pages/ResetPassword'
-import Reports from './pages/Reports'
-import Disponibility from './pages/Disponibility'
-import Layout from './pages/Layout'
-
+import "./App.css";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { BookingProvider } from "./hooks/bookingContext";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { Route, Routes } from "react-router";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { UserProvider } from "./hooks/userContext";
+import Booking from "./pages/Event";
+import Club from "./pages/Club";
+import Disponibility from "./pages/Disponibility";
+import EventCreate from "./pages/EventCreate";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Login from "./pages/Login";
+import LoginSuccess from "./pages/LoginSuccess";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+import Reports from "./pages/Reports";
+import ResetPassword from "./pages/ResetPassword";
+import Slots from "./pages/Slots";
 
 function App() {
   const theme = createTheme({
     palette: {
-      mode: 'dark',
+      mode: "dark",
       primary: {
-        main: '#C3FD74',
+        main: "#C3FD74",
       },
       secondary: {
-        main: '#3FEBBD',
+        main: "#3FEBBD",
       },
       warning: {
-        main: '#ed6c02',
+        main: "#ed6c02",
       },
       background: {
-        paper: '#03081B',
-        default: '#03081B',
+        paper: "#03081B",
+        default: "#03081B",
       },
     },
     components: {
       MuiInputBase: {
         styleOverrides: {
           label: {
-            color: 'white'
+            color: "white",
           },
           root: {
-            backgroundColor: "rgb(255,255,255, 0.1)", 
-            '& fieldset': {
-                borderColor: 'white',
+            backgroundColor: "rgb(255,255,255, 0.1)",
+            "& fieldset": {
+              borderColor: "white",
             },
-            '& label': {
-              color: 'white'
-            }
-          }
-        }
+            "& label": {
+              color: "white",
+            },
+          },
+        },
       },
-    }
-  })
+    },
+  });
 
   return (
     <>
       <ThemeProvider theme={theme}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <UserProvider>
-            <BookingProvider>  
-            <Layout>
-            <div class='content'>
-              <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/club/:id' element={<Club />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/login-success' element={<LoginSuccess />} />
-                <Route path='/register' element={<Register />} />
-                <Route path='/event/:id' element={<Booking />} />
-                <Route path='/new-event' element={<EventCreate/>}/>
-                <Route path='/disponibility' element={<Disponibility/>}/>
-                <Route path='/profile' element={<Profile/>}/>
-                <Route path='/new-slot/:id' element={<Slots />}/>
-                <Route path='/forgot-password' element={<ForgotPassword/>}/>
-                <Route path='/reset-password' element={<ResetPassword/>}/>
-                <Route path='/reports' element={<Reports/>}/>
-              </Routes>
-            </div>
-            </Layout>
-          </BookingProvider>
+            <BookingProvider>
+              <Layout>
+                <div class="content">
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/club/:id" element={<Club />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/login-success" element={<LoginSuccess />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/event/:id" element={<Booking />} />
+                    <Route path="/new-event" element={<EventCreate />} />
+                    <Route path="/disponibility" element={<Disponibility />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/new-slot/:id" element={<Slots />} />
+                    <Route
+                      path="/forgot-password"
+                      element={<ForgotPassword />}
+                    />
+                    <Route path="/reset-password" element={<ResetPassword />} />
+                    <Route path="/reports" element={<Reports />} />
+                  </Routes>
+                </div>
+              </Layout>
+            </BookingProvider>
           </UserProvider>
         </LocalizationProvider>
       </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
