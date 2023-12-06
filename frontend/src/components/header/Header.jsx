82cb63142e112a0f672e6426c83a333c2cabef80
mef <ferraro.mariaeugenia@gmail.com>
2023-12-06 00:33:00 -0300

    club bookings styles and redirection to home when logout

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -127,7 +127,9 @@ const Header = () => {
               > <Link to={link} style={{textDecoration: 'none', color: 'inherit'}}>
                   <MenuItem sx={MenuListSX} onClick={handleMenuClose}>{menuOptions[0]}</MenuItem>
                 </Link>
-                <MenuItem sx={MenuListSX} onClick={() => logoutUser()}>{menuOptions[1]}</MenuItem>
+                <Link to='/' style={{textDecoration: 'none', color: 'inherit'}}>
+                  <MenuItem sx={MenuListSX} onClick={() => logoutUser()}>{menuOptions[1]}</MenuItem>
+                </Link>
               </Menu>
             </>
 
--- frontend/src/pages/Bookings.jsx
+++ frontend/src/pages/Bookings.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Paper, Typography, Table, TableHead, TableRow, TableCell, TableBody, Collapse, List, ListItem, ListItemText, IconButton } from '@mui/material';
+import { Box, Container, Paper, Typography, Table, TableHead, TableRow, TableCell, TableBody, Collapse, List, ListItem, ListItemText, IconButton } from '@mui/material';
 import { ExpandMore as ExpandMoreIcon, ExpandLess as ExpandLessIcon } from '@mui/icons-material';
+import { PaperSXX, BoxSX } from "../components/customMui/CustomMui";
 import axiosInstance from "../hooks/api/axiosConfig";
+
 
 const Bookings = () => {
   const { id } = useParams();
@@ -28,46 +30,54 @@ const Bookings = () => {
   };
 
   return (
-    <Container>
-      <Paper>
-        <Typography variant="h5" align="center" gutterBottom>
-          Reservas del Club 
-        </Typography>
+    <Container sx={{ mb: 2 }}>
+      <Paper sx={PaperSXX}>
+        <Box sx={BoxSX}>
+          <Typography variant="h5" color="primary.main">
+            Reservas del Club 
+          </Typography>
+          </Box>
+        {/* <Typography variant="h5" align="center" gutterBottom>
+        </Typography> */}
         <Table>
           <TableHead>
-            <TableRow>
-              <TableCell>Nombre</TableCell>
-              <TableCell>Actividad</TableCell>
-              <TableCell>Fecha</TableCell>
-              <TableCell>Hora de inicio</TableCell>
-              <TableCell>Hora de fin</TableCell>
-              <TableCell>Mensaje</TableCell>
-              <TableCell>Participantes</TableCell>
+            <TableRow sx={{backgroundColor:'background.default'}}>  
+              <TableCell><Typography variant="h5" color="secondary.main" sx={{fontSize:'16px'}}>Nombre Reserva</Typography></TableCell>
+              <TableCell><Typography variant="h5" color="secondary.main" sx={{fontSize:'16px'}}>Actividad</Typography></TableCell>
+              <TableCell><Typography variant="h5" color="secondary.main" sx={{fontSize:'16px'}}>Fecha</Typography></TableCell>
+              <TableCell><Typography variant="h5" color="secondary.main" sx={{fontSize:'16px'}}>Hora de inicio</Typography></TableCell>
+              <TableCell><Typography variant="h5" color="secondary.main" sx={{fontSize:'16px'}}>Hora de fin</Typography></TableCell>
+              <TableCell><Typography variant="h5" color="secondary.main" sx={{fontSize:'16px'}}>Mensaje</Typography></TableCell>
+              <TableCell><Typography variant="h5" color="secondary.main" sx={{fontSize:'16px'}}>Participantes</Typography></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {bookings.map((booking) => (
               <React.Fragment key={booking.id}>
-                <TableRow>
+                <TableRow sx={{ height: expandedBookingId === booking.id ? 'auto' : 'inherit' }}>
                   <TableCell>{booking.name}</TableCell>
                   <TableCell>{booking.activityName}</TableCell>
                   <TableCell>{booking.date}</TableCell>
                   <TableCell>{booking.startTime}</TableCell>
                   <TableCell>{booking.endTime}</TableCell>
-                  <TableCell>{booking.message}</TableCell>
+                  <TableCell sx={{ overflow: 'auto'}}>{booking.message}</TableCell>
                   <TableCell>
-                    <IconButton onClick={() => handleExpandBooking(booking.id)}>
+                    <IconButton 
+                      onClick={() => handleExpandBooking(booking.id)}
+                      disabled={booking.participants.length === 0}
+                    >
                       {expandedBookingId === booking.id ? <ExpandLessIcon /> : <ExpandMoreIcon />}
                     </IconButton>
+                    {booking.participants.length}
                   </TableCell>
                 </TableRow>
-                <TableRow>
+                <TableRow sx={{ display: expandedBookingId === booking.id ? 'table-row' : 'none' }}>
                   <TableCell colSpan={7}>
                     <Collapse in={expandedBookingId === booking.id} timeout="auto" unmountOnExit>
                       <List>
                         {booking.participants.map((participant) => (
-                          <ListItem key={participant.id}>
-                            <ListItemText primary={`${participant.firstName} ${participant.lastName} - ${participant.email}`} />
+                          <ListItem key={participant.id} sx={{ textAlign: 'right', fontStyle:'italic' }}>
+                            <ListItemText primary={ <Typography color="secondary.main" sx={{ fontSize: '14px' }}>{participant.firstName} {participant.lastName} - {participant.email}</Typography>} />
                           </ListItem>
                         ))}
                       </List>
