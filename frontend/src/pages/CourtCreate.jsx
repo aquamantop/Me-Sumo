f83ecf8b3b0eca25419d2249b45c7c6d8ab08150
mef <ferraro.mariaeugenia@gmail.com>
2023-12-09 14:33:57 -0300

    reset form

--- frontend/src/pages/CourtCreate.jsx
+++ frontend/src/pages/CourtCreate.jsx
@@ -14,9 +14,7 @@ import {
     Accordion,
     AccordionDetails,
     AccordionSummary,
-    Alert,
     List,
-    ListItem,
   } from "@mui/material";
 import { PaperSXX, BoxSX, ButtonSX } from "../components/customMui/CustomMui";
 import AccessibilityNewOutlinedIcon from '@mui/icons-material/AccessibilityNewOutlined';
@@ -47,6 +45,7 @@ const CreateCourt = () => {
     const [isButtonEnabled, setButtonEnabled] = useState(false);
     const [clubData, setClubData] = useState(null);
     const [showMessage, setShowMessage] = useState(false);
+    const [resetForm, setResetForm] = useState(false);
     const [courtInfo, setCourtInfo] = useState({
         name: null,
         activity:null,
@@ -63,15 +62,14 @@ const CreateCourt = () => {
           } catch (error) {
             console.error('Error fetching club data:', error);
           }
-        };
-    
+        };    
         fetchData();
     }, []);
     
     
     const groupCourtsByActivity = () => {
         if (!clubData) return {};
-    
+   
         return clubData.reduce((groupedCourts, court) => {
           const key = `${court.activity.name} ${court.activity.type}`;
     
@@ -85,14 +83,14 @@ const CreateCourt = () => {
         }, {});
     };
     
-
+    
     const groupedCourts = groupCourtsByActivity();
+    
     
     const sortedActivityKeys = Object.keys(groupedCourts).sort((a, b) => {
         const [nameA, typeA] = a.split(' ');
         const [nameB, typeB] = b.split(' ');
     
-
         if (nameA === nameB) {
           return parseInt(typeA) - parseInt(typeB);
         } else {
@@ -115,14 +113,15 @@ const CreateCourt = () => {
             const response = await axiosInstance.post('/court/add', body,
              {headers: {
                  "Authorization": `Bearer ${user.token}`
-              }}
-            
-            
+              }}        
             );        
+            
             console.log(response.data);
             const updatedResponse = await axiosInstance.get(`/court/club/${user.clubId}`);
             setClubData(updatedResponse.data);
             setShowMessage(true);
+            setResetForm(true);  
+        
         } catch (error) {
             console.error('Error al crear la cancha:', error);
         }
@@ -130,6 +129,7 @@ const CreateCourt = () => {
 
 
     const handleInputChange = (fieldName) => (event, value) => {
+        
         let fieldValue = value || event.target.value;
     
         if (fieldName === 'activity') {
@@ -165,11 +165,21 @@ const CreateCourt = () => {
     }, [courtInfo]);
 
 
+    const resetFormValues = () => {
+        setResetForm(false);
+        setCourtInfo({
+            name: null,
+            activity: null,
+            court: null,
+            inside: null,
+        });
+    };
+
     return (
         <>
         {true ? (
         false ? (<CustomLoader />) : (
-            <Container sx={{ mb: 2 }}>
+            <Container sx={{ mb: 2 }} key={resetForm}>
             <Paper sx={PaperSXX}>
                 <Box sx={{ ...BoxSX }}>
                 <Typography variant="h5" color="primary.main">
@@ -183,34 +193,35 @@ const CreateCourt = () => {
                         Datos de la cancha a crear
                     </Typography>
                     <Divider sx={{ mt: 1, mb: 2 }} />
+
                     <Grid container spacing={2}>
 
                         <Grid item xs={6}>
-                        <Autocomplete
-                            id="name"
-                            disablePortal
-                            fullWidth
-                            freeSolo
-                            options={options}
-                            onInputChange={(event, value) => handleInputChange("name")(event, value)}
-                            renderInput={(params) => (
-                                <TextField
-                                {...params}
-                                label="Crear Nombre"
-                                id="custom-css-outlined-input"
-                                sx={{ mt: 2 }}
-                                InputProps={{
-                                    ...params.InputProps,
-                                    startAdornment: (
-                                    <InputAdornment position="start">
-                                        <IconButton>
-                                        <StadiumOutlinedIcon />
-                                        </IconButton>
-                                    </InputAdornment>
-                                    ),
-                                }}
-                                />
-                            )}
+                            <Autocomplete
+                                id="name"
+                                disablePortal
+                                fullWidth
+                                freeSolo
+                                options={options}
+                                onInputChange={(event, value) => handleInputChange("name")(event, value)}
+                                renderInput={(params) => (
+                                    <TextField
+                                    {...params}
+                                    label="Crear Nombre"
+                                    id="custom-css-outlined-input"
+                                    sx={{ mt: 2 }}
+                                    InputProps={{
+                                        ...params.InputProps,
+                                        startAdornment: (
+                                        <InputAdornment position="start">
+                                            <IconButton>
+                                            <StadiumOutlinedIcon />
+                                            </IconButton>
+                                        </InputAdornment>
+                                        ),
+                                    }}
+                                    />
+                                )}
                             />
                         </Grid>
                         
@@ -275,31 +286,31 @@ const CreateCourt = () => {
 
 
                         <Grid item xs={6} sx={{ marginBottom: 2 }}>
-                        <Autocomplete
-                            id="inside"
-                            disablePortal
-                            fullWidth
-                            options={Object.values(inside)}
-                            getOptionLabel={(option) => option || ''}
-                            onChange={(event, value) => handleInputChange("inside")(event, value)}
-                            renderInput={(params) => (
-                                <TextField
-                                {...params}
-                                label="¿Es techada?"
-                                id="custom-css-outlined-input"
-                                sx={{ mt: 2 }}
-                                InputProps={{
-                                    ...params.InputProps,
-                                    startAdornment: (
-                                    <InputAdornment position="start">
-                                        <IconButton>
-                                        <OtherHousesOutlinedIcon />
-                                        </IconButton>
-                                    </InputAdornment>
-                                    ),
-                                }}
-                                />
-                            )}
+                            <Autocomplete
+                                id="inside"
+                                disablePortal
+                                fullWidth
+                                options={Object.values(inside)}
+                                getOptionLabel={(option) => option || ''}
+                                onChange={(event, value) => handleInputChange("inside")(event, value)}
+                                renderInput={(params) => (
+                                    <TextField
+                                    {...params}
+                                    label="¿Es techada?"
+                                    id="custom-css-outlined-input"
+                                    sx={{ mt: 2 }}
+                                    InputProps={{
+                                        ...params.InputProps,
+                                        startAdornment: (
+                                        <InputAdornment position="start">
+                                            <IconButton>
+                                            <OtherHousesOutlinedIcon />
+                                            </IconButton>
+                                        </InputAdornment>
+                                        ),
+                                    }}
+                                    />
+                                )}
                             />
                         </Grid>
                     </Grid>
@@ -350,7 +361,10 @@ const CreateCourt = () => {
                 open={showMessage}
                 title="Cancha Creada"
                 message="¡La cancha se ha creado con éxito!"
-                onClose={() => setShowMessage(false)}
+                onClose={() => {
+                    setShowMessage(false);
+                    resetFormValues();
+                }}
             />
             </Container>
         )
