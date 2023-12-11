9d17ace35755edf92044b53f0f35728d1e04a9d4
nahuelranieri <nnranieri.ok@gmail.com>
2023-12-11 01:18:19 -0300

    feat: hamburguer menu, side menu fix: colors on footer

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -12,6 +12,10 @@ import {
   MenuItem,
   Paper,
   Tooltip,
+  Drawer,
+  Container,
+  Divider,
+  IconButton
 } from '@mui/material';
 import { useMediaQuery } from '@mui/material';
 import logoDesktop from '../../assets/logoDesktop2.svg';
@@ -21,6 +25,8 @@ import { useUserContext } from '../../hooks/userContext';
 import { BoxSX, MenuListSX, PaperSXX } from '../customMui/CustomMui';
 import { useLocation } from 'react-router-dom';
 import axiosInstance from '../../hooks/api/axiosConfig';
+import { Logout, Portrait, Assessment, Upload, Book, Login, Face} from '@mui/icons-material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -32,12 +38,14 @@ const Header = () => {
   ]);
   const location = useLocation();
   const [userInfo, setUserInfo] = useState({});
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
 
   const fetchAndSetUserInfo = async (email) => {
     try {
       const userResponse = await axiosInstance.get(`/user/search-email?email=${email}`);
-      const { firstName } = userResponse.data;
-      setUserInfo({ firstName });
+      const { firstName, lastName, userName } = userResponse.data;
+      setUserInfo({ firstName, lastName, userName });
     } catch (error) {
       console.error('Error fetching user information:', error);
     }
@@ -57,8 +65,8 @@ const Header = () => {
       ];
     } else if (role === 'ROLE_USER') {
       return [
-        { label: 'Mi Perfil', link: '/profile' },
-        { label: 'Cerrar Sesión', link: '/' },
+        { label: 'Mi Perfil', link: '/profile', icon: 'Portrait' },
+        { label: 'Cerrar Sesión', link: '/', icon: 'Logout' },
       ];
     } else {
       return [];
@@ -81,6 +89,14 @@ const Header = () => {
 
   const handleMenuClose = () => {
     setAnchorEl(null);
+  };
+
+  const handleDrawerOpen = () => {
+    setIsDrawerOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setIsDrawerOpen(false);
   };
 
   const handleMenuCloseAndNavigate = (link) => {
@@ -150,44 +166,171 @@ const Header = () => {
                 <Typography variant='body1' color='primary.main' align='inherit'>
                   ¡Hola {userInfo.firstName}!
                 </Typography>
-                <Avatar onClick={handleMenuOpen} sx={{ cursor: 'pointer' }}></Avatar>
+                <Avatar onClick={handleDrawerOpen} sx={{ cursor: 'pointer', bgcolor: 'primary.main' }}>{userInfo.firstName?.[0] ?? ''}</Avatar>
+                {/* <Avatar onClick={handleMenuOpen} sx={{ cursor: 'pointer' }}></Avatar> */}
               </Box>
-              <Menu
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={handleMenuClose}
-              >
-                {menuInfo.map((option) => (
-                  <Link
-                    to={option.link}
-                    style={{ textDecoration: 'none', color: 'inherit' }}
-                    key={option.label}
+              <Drawer 
+                anchor="right" 
+                open={isDrawerOpen} 
+                onClose={handleDrawerClose}
+                sx={{
+                  backdropFilter: 'blur(3px)',
+                  WebkitBackdropFilter: 'blur(3px)',
+                          }}
+                PaperProps={{
+                  sx:{
+                    borderLeft: "2px solid",
+                    borderColor: "secondary.main",
+                    // background: "linear-gradient(180deg, #0D2430 0%, rgba(13, 36, 48, 0) 100%)",
+                    background: 'rgba(13, 36, 48, 0.95)'
+                  }
+                  
+                }}
+                >
+                  <Container
+                    role="presentation"
+                    onClick={handleDrawerClose}
+                    onKeyDown={handleDrawerClose}
+                    maxWidth={false}
+                    sx={{width: '270px', pl: '0px!important', pr: '0px!important'}}
                   >
-                    <MenuItem
-                      sx={MenuListSX}
-                      onClick={
-                        option.label === 'Cerrar Sesión' ? handleLogout : handleMenuClose
-                      }
+                    <Box
+                      sx={{
+                        height:'71.5px',
+                        display: 'flex',
+                        justifyContent: 'space-evenly'
+                      }}
+                      p={2}
                     >
-                      {option.label}
-                    </MenuItem>
-                  </Link>
-                ))}
-              </Menu>
+                      <Avatar 
+                        sx={{
+                          bgcolor: 'secondary.main',
+                          width: 50,
+                          height: 50
+                          }}>{userInfo.firstName?.[0] ?? ''}</Avatar>
+                      <Box>
+                        <Typography variant='h6' sx={{my:1}} color='secondary'>{userInfo.firstName} {userInfo.lastName}</Typography>
+                        <Typography variant='h6' color='secondary'>@{userInfo.userName}</Typography>
+                      </Box>
+
+
+                    </Box>
+                    <Divider sx={{ my: 1 }} />
+                    {menuInfo.map((option) => (
+                      <Link
+                        to={option.link}
+                        style={{ textDecoration: 'none', color: 'inherit' }}
+                        key={option.label}
+                      >
+                        <MenuItem
+                          sx={{mb:1}}
+                          onClick={
+                            option.label === 'Cerrar Sesión' ? handleLogout : handleDrawerClose
+                          }
+                        >
+                          {option.label === 'Reservas' && <Book sx={{mr:1}}/>} 
+                          {option.label === 'Cargar Cancha' && <Upload sx={{mr:1}}/>} 
+                          {option.label === 'Reporte' && <Assessment sx={{mr:1}}/>} 
+                          {option.label === 'Mi Perfil' && <Portrait sx={{mr:1}}/>} 
+                          {option.label === 'Cerrar Sesión' && <Logout sx={{mr:1}}/>} 
+                          <Typography variant='h6'>{option.label}</Typography>
+                        </MenuItem>
+                      </Link>
+                    ))}
+                  </Container>
+                </Drawer>
             </>
           ) : (
-            <>
-              <Link to='/register'>
-                <Button variant='outlined' sx={{ ml: 2 }}>
-                  Registrarse
+            isMobile ? (
+              <>
+              <Box
+                sx={{
+                  ...BoxSX,
+                  borderColor: 'primary.main',
+                  p: 0
+                }}
+              >
+                <Button 
+                  sx={{ p: 1}}
+                  onClick={handleDrawerOpen}
+                >
+                  <MenuIcon/>
                 </Button>
-              </Link>
-              <Link to='/login'>
-                <Button variant='outlined' sx={{ ml: 2 }}>
-                  Iniciar sesión
-                </Button>
-              </Link>
-            </>
+              </Box>
+              <Drawer
+                open={isDrawerOpen} 
+                onClose={handleDrawerClose}
+                anchor="right"
+                sx={{
+                  backdropFilter: 'blur(3px)',
+                  WebkitBackdropFilter: 'blur(3px)',
+                          }}
+                PaperProps={{
+                  sx:{
+                    borderLeft: "2px solid",
+                    borderColor: "secondary.main",
+                    // background: "linear-gradient(180deg, #0D2430 0%, rgba(13, 36, 48, 0) 100%)",
+                    background: 'rgba(13, 36, 48, 0.95)'
+                  }
+                  
+                }}
+              >
+                <Container
+                    role="presentation"
+                    onClick={handleDrawerClose}
+                    onKeyDown={handleDrawerClose}
+                    maxWidth={false}
+                    sx={{width: '270px', pl: '0px!important', pr: '0px!important'}}
+                  >
+                    <Box
+                      sx={{
+                        height:'103.5px',
+                        display: 'flex',
+                        alignItems: 'flex-end'
+                      }}
+                      p={2}
+                      
+                    >
+                      
+                      <Typography variant='h5' color='secondary' >Menú</Typography>
+
+                    </Box>
+                    <Divider sx={{ my: 1}} />
+                      <Link
+                        to='/login'
+                        style={{ textDecoration: 'none', color: 'inherit' }}
+                      >
+                        <MenuItem sx={{mb:1}}>
+                          <Login sx={{mr: 1}}/>
+                          <Typography variant='h6'>Iniciar sesión</Typography>
+                        </MenuItem>
+                      </Link>
+                      <Link
+                        to='/register'
+                        style={{ textDecoration: 'none', color: 'inherit' }}
+                      >
+                        <MenuItem sx={{mb:1}}>
+                          <Face sx={{mr: 1}}/>
+                          <Typography variant='h6' >Registrase</Typography>
+                        </MenuItem>
+                      </Link>
+                  </Container>
+              </Drawer>
+              </>
+            ) : (
+              <>
+                <Link to='/register'>
+                  <Button variant='outlined' sx={{ ml: 2 }}>
+                    Registrarse
+                  </Button>
+                </Link>
+                <Link to='/login'>
+                  <Button variant='outlined' sx={{ ml: 2 }}>
+                    Iniciar sesión
+                  </Button>
+                </Link>
+              </>
+            )
           )}
         </Box>
       </Toolbar>
