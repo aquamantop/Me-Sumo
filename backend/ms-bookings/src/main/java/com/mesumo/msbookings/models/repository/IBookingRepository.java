fb0d07f5976d96d67124ad7e12d41146a4d7c978
Franco Rampazzo <franco.rodrigo.rampazzo@gmail.com>
2023-12-13 15:57:45 -0300

    fix: update all participant with same userId on user update

--- backend/ms-bookings/src/main/java/com/mesumo/msbookings/models/repository/IBookingRepository.java
+++ backend/ms-bookings/src/main/java/com/mesumo/msbookings/models/repository/IBookingRepository.java
@@ -3,6 +3,8 @@ package com.mesumo.msbookings.models.repository;
 import com.mesumo.msbookings.models.entities.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IBookingRepository extends JpaRepository<Booking, Long>, JpaSpecificationExecutor<Booking> {
 }
--- backend/ms-bookings/src/main/java/com/mesumo/msbookings/models/repository/IParticipantRepository.java
+++ backend/ms-bookings/src/main/java/com/mesumo/msbookings/models/repository/IParticipantRepository.java
@@ -2,10 +2,12 @@ package com.mesumo.msbookings.models.repository;
 
 import com.mesumo.msbookings.models.entities.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
 public interface IParticipantRepository extends JpaRepository<Participant, Long> {
 
-    Optional<Participant> findByUserId(Long userId);
+    List<Participant> findByUserId(Long userId);
 
 }
